1432bb38ae7d4c3c5fe8cf947dd28ea8d8c1c4a0
Ankitapatil <ankitakhairnar10@gmail.com>
2021-03-01 21:13:18 +0530

    Added: Store the resultin Dictionary

--- arithmaticcomputation.sh
+++ arithmaticcomputation.sh
@@ -1,5 +1,14 @@
 #!/bin/bash -x
-echo "Enter the three inputs :: "
+echo "Enter three inputs :: " 
 read a b c
-result1=$(($a%$b+$c))
-echo $result1
+declare -A Dict
+Dict[result1]=$(($a+$b*$c))
+Dict[result2]=$(($a*$b+$c))
+Dict[result3]=$(($c+$a/$b))
+Dict[result4]=$(($a%$b+$c))
+
+echo "First computation is :  " ${Dict[result1]} 
+echo "Second computation is : " ${Dict[result2]}
+echo "Third computation is :  " ${Dict[result3]}
+echo "Fourth computation is : " ${Dict[result4]}
+echo ${Dict[@]}
